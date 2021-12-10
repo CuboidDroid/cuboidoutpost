1f7ef38d86b6fcee074f514a676aea2f2c0d8e22
CuboidDroid <pokedandwent@gmail.com>
2021-12-10 22:49:17 +0000

    v1.0.5

--- src/server files/settings.sh
+++ src/server files/settings.sh
@@ -1,5 +1,5 @@
 # Don't edit these values unless you know what you are doing.
-export SERVER_JAR="forge-1.16.5-36.2.8.jar"
+export SERVER_JAR="forge-1.16.5-36.2.20.jar"
 
 # You should edit these values to match your server setup
 export MIN_RAM="2048M"
